ea98e8b176c8ed52cef34a6431a2e5cc094f298c
joselinares17 <josedaniellinares135@gmail.com>
2024-05-23 17:52:58 -0500

    Initial commit

---
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/RepositorioTpiApplication.java
@@ -0,0 +1,13 @@
+package org.lumeninvestiga.backend.repositorio.tpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioTpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioTpiApplication.class, args);
+	}
+
+}
