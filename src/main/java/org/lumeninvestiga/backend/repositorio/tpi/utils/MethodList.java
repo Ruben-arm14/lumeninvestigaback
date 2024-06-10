b8e868575d66445c624b833b9763b836ab6c36d3
joselinares17 <josedaniellinares135@gmail.com>
2024-06-10 18:06:36 -0500

    Adding PDFBox dependency

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/utils/MethodList.java
+++
@@ -1,7 +0,0 @@
-package org.lumeninvestiga.backend.repositorio.tpi.utils;
-
-//TODO: Revisar aplicación en las clases Entity.
-public interface MethodList {
-    <T> void add(T element);
-    <T> void remove(T element);
-}
