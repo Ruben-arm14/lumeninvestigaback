e4069aa5380325cb646bb3943f78c3a1e5ef4176
joselinares17 <josedaniellinares135@gmail.com>
2024-06-30 23:16:54 -0500

    Modifying file controller

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/controllers/FileController.java
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/controllers/FileController.java
@@ -22,57 +22,41 @@ public class FileController {
 
     @PostMapping(consumes = "multipart/form-data", produces = "application/json")
     public ResponseEntity<?> createFile(@RequestParam("file") MultipartFile file) {
-        Optional<File> fileOptional = fileService.saveFile(file);
-        if (fileOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(fileOptional.get());
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error saving file");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
     public ResponseEntity<?> readAllFiles(@PageableDefault Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(fileService.getAllFiles());
+        return ResponseEntity.status(HttpStatus.OK).body(fileService.getAllFiles(pageable));
     }
 
     @GetMapping("by/{name}")
     public ResponseEntity<?> readByName(@PathVariable String name) {
-        Optional<File> fileOptional = fileService.getFileByName(name);
-        if(fileOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.FOUND).body(fileOptional);
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.FOUND).build();
+        //return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> readFileById(@PathVariable Long id) {
-        Optional<File> fileOptional = fileService.getFileById(id);
-        if (fileOptional.isPresent()) {
-            return ResponseEntity.ok(fileOptional.get());
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
     //Solo se puede modificar el nombre, aún falta arreglar
     @PutMapping("/{name}")
     public ResponseEntity<?> updateFileById(@PathVariable Long id, @RequestParam("name") File file) {
-        Optional<File> fileOptional = fileService.getFileById(id);
-        if (fileOptional.isPresent()) {
-            File fileDb = fileOptional.get();
-            fileDb.setName(file.getName());
-            //TODO: cambiar a un dto con un mapper
-            fileService.saveFile((MultipartFile) fileDb);
-            return ResponseEntity.status(HttpStatus.CREATED).body(fileDb);
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//        Optional<File> fileOptional = fileService.getFileById(id);
+//        if (fileOptional.isPresent()) {
+//            File fileDb = fileOptional.get();
+//            fileDb.setName(file.getName());
+//            //TODO: cambiar a un dto con un mapper
+//            fileService.saveFile((MultipartFile) fileDb);
+//            return ResponseEntity.status(HttpStatus.CREATED).body(fileDb);
+//        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteFileById(@PathVariable Long id){
-        Optional<File> fileOptional = fileService.getFileById(id);
-        if(fileOptional.isPresent()) {
-            fileService.deleteFileById(id);
-            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 }
