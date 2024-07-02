194c52d1e6f9288d116d9fc8fd86ce2348067fd6
joselinares17 <josedaniellinares135@gmail.com>
2024-07-02 12:59:08 -0500

    Adding new validation

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/dto/request/UserLoginRequest.java
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/dto/request/UserLoginRequest.java
@@ -1,12 +1,15 @@
 package org.lumeninvestiga.backend.repositorio.tpi.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginRequest(
         @Email(message = "invalid email address")
+                @NotNull
         String email,
         @Size(min = 8, message = "password must be at least 8 characters long")
+                @NotNull
         String password
 ) {
 }
