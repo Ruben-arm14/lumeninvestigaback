7d43b294f7b4a2267a8a026be97a7aee2a3cdc95
joselinares17 <josedaniellinares135@gmail.com>
2024-07-03 13:02:18 -0500

    Adding new query

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/repositories/ArticleRepository.java
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/repositories/ArticleRepository.java
@@ -2,12 +2,18 @@ package org.lumeninvestiga.backend.repositorio.tpi.repositories;
 
 import org.lumeninvestiga.backend.repositorio.tpi.entities.data.Article;
 import org.lumeninvestiga.backend.repositorio.tpi.entities.data.File;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByName(String name);
+    @Query("SELECT a FROM Article a WHERE a.title LIKE %:keyword%")
+    List<Article> findByTitleContaining(Pageable pageable, @Param("keyword") String keyword);
 }
