cf7c0ecb1daf5ca84b91f0602fd605821e0cfe2c
joselinares17 <josedaniellinares135@gmail.com>
2024-07-06 17:46:05 -0500

    Adding new static method to format academics periods

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/utils/Utility.java
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/utils/Utility.java
@@ -2,6 +2,9 @@ package org.lumeninvestiga.backend.repositorio.tpi.utils;
 
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Utility {
     public static int getCurrentPage(Pageable pageable) {
         int page = pageable.getPageNumber();
@@ -9,6 +12,24 @@ public class Utility {
             page -=1;
         }
         return page;
+    }
+
+    public static String calculatePeriod(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(dateString, formatter);
+
+        int year = date.getYear();
+        int month = date.getMonthValue();
+
+        if (month <= 2) {
+            return year + "-0";
+        } else if (month >= 4 && month <= 7) {
+            return year + "-1";
+        } else if (month >= 8 && month <= 12) {
+            return year + "-2";
+        } else {
+            return "No válido";
+        }
     }
 
     private Utility() {
