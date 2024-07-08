49bd37bfebd73658b8db029b8268d94efbca80e7
joselinares17 <josedaniellinares135@gmail.com>
2024-07-07 23:47:45 -0500

    updating

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/dto/request/AuthorizationRequest.java
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/dto/request/AuthorizationRequest.java
@@ -2,15 +2,14 @@ package org.lumeninvestiga.backend.repositorio.tpi.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record UserLoginRequest(
+public record AuthorizationRequest(
         @Email(message = "invalid email address")
-                @NotEmpty
+            @NotEmpty
         String email,
         @Size(min = 8, message = "password must be at least 8 characters long")
-                @NotEmpty
+            @NotEmpty
         String password
 ) {
 }
---
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,90 @@
+package org.lumeninvestiga.backend.repositorio.tpi.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.lumeninvestiga.backend.repositorio.tpi.dto.request.AuthorizationRequest;
+import org.lumeninvestiga.backend.repositorio.tpi.dto.request.UserLoginRequest;
+import org.lumeninvestiga.backend.repositorio.tpi.entities.user.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.lumeninvestiga.backend.repositorio.tpi.security.TokenJwtConfig.*;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            AuthorizationRequest userCredentials = new ObjectMapper()
+                    .readValue(request.getInputStream(), AuthorizationRequest.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                        userCredentials.email(), userCredentials.password()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        String username = user.getUsername();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+        Claims claims = Jwts.claims()
+                .add("authorities", roles)
+                .add("code", code)
+                .build();
+
+        String token = Jwts.builder()
+                .subject(code)
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .issuedAt(new Date())
+                .signWith(SECRET_KEY)
+                .compact();
+
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
+        body.put("code", code);
+        body.put("message", String.format("Hola %s has iniciado sesión con éxito!", code));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(200);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        Map<String, String> body = new HashMap<>();
+        body.put("message", "Error en la autenticación, email o password incorrectos!");
+        body.put("error", failed.getMessage());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(401);
+    }
+}
